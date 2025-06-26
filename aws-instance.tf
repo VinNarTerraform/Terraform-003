21ec39b292a55e147a714ad8c06c873e23542d82
Vinod Narayanan <133094722+vinodnara@users.noreply.github.com>
2025-06-26 18:16:51 +0300

    Update aws-instance.tf

--- aws-instance.tf
+++ aws-instance.tf
@@ -2,13 +2,8 @@ provider "aws" {
   region ="us-east-1"
 }
 
-
 resource "aws_instance" "mysec1" {
   ami   = "ami-09e6f87a47903347c"
   instance_type = "t2.micro"
-
-    tags = {
-      Name: "My-First-ec2"
-  }
 }
 
